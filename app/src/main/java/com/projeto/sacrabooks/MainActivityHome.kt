3fb8db5bdaebe3f2ef5ea1aeacc1ed44f25d72e4
Vanny <vanny11fm@gmail.com>
2024-09-13 20:01:51 -0300

    Intent login.

--- app/src/main/java/com/projeto/sacrabooks/MainActivityHome.kt
+++ app/src/main/java/com/projeto/sacrabooks/MainActivityHome.kt
@@ -1,5 +1,6 @@
 package com.projeto.sacrabooks
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
@@ -10,20 +11,25 @@ import com.projeto.sacrabooks.R
 class MainActivityHome : AppCompatActivity() {
 
     private lateinit var prosseguir: ImageView
+    private lateinit var login: ImageView
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_home)
 
         prosseguir = findViewById(R.id.imageViewCafe)
+        login = findViewById(R.id.imageViewLogin)
 
         prosseguir.setOnClickListener {
-
             val proximaTela = Intent(this, MainActivityDetalhes::class.java)
-
             startActivity(proximaTela)
+        }
 
+        login.setOnClickListener {
+            val proximaTela = Intent(this, MainActivityLogin::class.java)
+            startActivity(proximaTela)
         }
     }
 }
